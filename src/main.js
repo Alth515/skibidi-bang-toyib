fd9f58526eaa0059e43c3727787d4cce8e9b8d06
Umar Alfarouk <medrivia@gmail.com>
2025-11-26 15:28:23 +0700

    fix: Component not found

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 // Entry point: create the App and mount it to #app
+import { mount } from 'svelte'
 import App from './App.svelte';
 
-const app = new App({
-  target: document.getElementById('app')
-});
+const app = mount(App, {target: document.getElementById('app')});
 
 export default app;
